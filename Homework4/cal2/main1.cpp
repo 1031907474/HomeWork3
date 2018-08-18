4e7b856b38e058f6a5a2f775e8a7261d2d134d6f
Qiuyan Wang <1031907474@qq.com>
2018-08-18 21:23:27 +0800

    “ file was deletedd,homework4 upload

---
+++ Homework4/cal2/main1.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define maxsize 1024
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "homework.h"
+using namespace std;
+
+int main()
+{
+	menu();
+	cal();
+	system("pause");
+	return 0;
+}
